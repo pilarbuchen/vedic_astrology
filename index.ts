503bd0ff6829423d21787179391c0fa83d91f3c6
pilarbuchen <pilarb@wix.com>
2023-05-23 09:11:36 -0500

    fetched data with axios

--- index.ts
+++ index.ts
@@ -12,43 +12,31 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-    const options = {
-        method: "POST",
-        url: "https://json.freeastrologyapi.com/planets",
+    axios.post('https://json.freeastrologyapi.com/planets', {
+        "year": 2022,
+        "month": 8,
+        "date": 11,
+        "hours": 6,
+        "minutes": 0,
+        "seconds": 0,
+        "latitude": 17.38333,
+        "longitude": 78.4666,
+        "timezone": 5.5,
+        "settings": {
+          "observation_point": "topocentric",
+          "ayanamsha": "lahiri"
+        }
+      }, {
         headers: {
             'Content-Type': 'application/json',
             'x-api-key': '4JTjqMmvgC2WeZfzglpFAaiYVxDaLmPI1I43Is1U'
-        },
-        body: JSON.stringify({
-            "year": 2022,
-            "month": 8,
-            "date": 11,
-            "hours": 6,
-            "minutes": 0,
-            "seconds": 0,
-            "latitude": 17.38333,
-            "longitude": 78.4666,
-            "timezone": 5.5,
-            "settings": {
-              "observation_point": "topocentric",
-              "ayanamsha": "lahiri"
-            }
-          })
-    };
-
-
-   
-    // axios.request(options).then((response) => {
-    //     res.json(response.data);
-    //     console.log(response.data)
-    // }).catch((error) => {
-    //     res.json(error);
-    // });
-
-    request(options, function (error, response) {
-        if (error) throw new Error(error);
-        console.log(response.body);
-      });
+        }
+      }).then((response) => {
+        res.json(response.data);
+        console.log(response.data)
+    }).catch((error) => {
+        res.json(error);
+    });
 });
 
 app.listen(PORT, () => {
