23731666a49416a740fb677fdc1113852e3e98b4
pilarbuchen <pilarb@wix.com>
2023-05-30 08:25:41 -0500

    added sign logic to planet list

--- src/components/SignList.tsx
+++ src/components/SignList.tsx
@@ -1,39 +1,47 @@
 import React from 'react';
 
 interface SignsProps {
- signs: {[key: string]: number | string}[]
+ signs: {planets: string, signs: string | number}[]
 }
 
 function SignList ({signs}: SignsProps) {
 
-// if (signs) {
-//   for (const [key, value] of Object.entries(signs)) {
-//     if (value.sign === 1) {
-//       value.sign = 'Aries'
-//     }
-//     console.log(value.sign)
-//   }
+if (signs) {
+  for (const [key, value] of Object.entries(signs)) {
+    if (value.signs === 1) {
+      value.signs = 'Aries'
+    } else if (value.signs === 2) {
+      value.signs = 'Taurus'
+    } else if (value.signs === 3) {
+      value.signs = 'Gemini'
+    } else if (value.signs === 4) {
+      value.signs = 'Cancer'
+    } else if (value.signs === 5) {
+      value.signs = 'Leo'
+    } else if (value.signs === 6) {
+      value.signs = 'Virgo'
+    } else if (value.signs === 7) {
+      value.signs = 'Libra'
+    } else if (value.signs === 8) {
+      value.signs = 'Scorpio'
+    } else if (value.signs === 9) {
+      value.signs = 'Sagittarius '
+    } else if (value.signs === 10) {
+      value.signs = 'Capricorn'
+    } else if (value.signs === 11) {
+      value.signs = 'Aquarius'
+    } else if (value.signs === 12) {
+      value.signs = 'Pisces'
+    }
   
-// }
-
-let astrologyOrder = {
-  1: 'Aries', 
-  2: 'Taurus',
-  3: 'Gemini ',
-  4: 'Cancer',
-  5: 'Leo',
-  6: 'Virgo',
-  7: 'Libra',
-  8: 'Scorpio',
-  9: 'Sagitarius',
-  10: 'Capricorn',
-  11: 'Aquarius',
-  12: 'Piscies'
+    console.log(value.signs)
+  }
+  
 }
 
         return (
           <>
-<>{signs?.map(p => p.signs )}</>
+<>{signs?.map(p => p.planets + p.signs)}</>
 </>
         );
         };
